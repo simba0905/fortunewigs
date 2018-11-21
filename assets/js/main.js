49bc60202258dd12a91443a1a7b1dffe3ba8dc17
simba0905 <rascalsimba0905@gmail.com>
2018-11-21 03:25:42 -0500

    popup

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,15 +18,16 @@ $(document).ready(function() {
 	$(id).css('left', winW/2-$(id).width()/2);
 
 	if (localStorage.getItem('popup') != 'loaded') {
-		//transition effect
 		$(id).fadeIn(2000); 
 		$('#mask').fadeIn(500);	
 		$('#mask').fadeTo("slow",0.9);
+		$( '#snow' ).fallingSnow();
 		localStorage.setItem('popup', 'loaded');
 	} else {
 		$(id).hide();
 		$('#mask').hide();
 		$('.window').hide();
+		$( '#snow' ).hide();
 	}
 
 	//if close button is clicked
@@ -36,23 +37,14 @@ $(document).ready(function() {
 		
 		$('#mask').hide();
 		$('.window').hide();
+		$( '#snow' ).hide();
 	});
 	
 	//if mask is clicked
 	$('#mask').click(function () {
 		$(this).hide();
 		$('.window').hide();
+		$( '#snow' ).hide();
 	});
 
-	  // Load cookie library
-  	$.getScript('https://cdn.rawgit.com/js-cookie/js-cookie/v2.1.2/src/js.cookie.js', function() {
-  		// If cookie found (already shown)
-	    if(Cookies.get('notice') !== undefined) {
-
-			// Hide cookie notice (change jQuery selector to match your own)
-			$('#mask').hide();
-			$('.window').hide();
-	    }
-  	});
-	
 });
